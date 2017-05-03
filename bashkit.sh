ba0d987d1fd91ba5b8b6f7510fd19cfde8716624
NullArray <NullArray@users.noreply.github.com>
2017-05-03 23:46:07 +0200

    Update bashkit.sh

--- bashkit.sh
+++ bashkit.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-# For now we'll keep the old ASCII art
-
 function usage()
 {	clear 2>/dev/null
-	echo " _____         _    _____ _ _   "   
-	echo "| __  |___ ___| |_ |  |  |_| |_ " 
-	echo "| __ -| .'|_ -|   ||    -| |  _|"
-	echo "|_____|__,|___|_|_||__|__|_|_|  "
-	printf "\nBash Kit - Multitool\n"
+	echo " _____ _____ ____  _____ "
+	echo "|     |     |    \|  _  |"
+	echo "| | | |-   -|  |  |     |"
+	echo "|_|_|_|_____|____/|__|__|"
+	printf "\nMIDA - Multitool\n"
 	
-	printf "\n Bash Kit - Multitool aims to be a comprehensive assistant for operations
+	printf "\n MIDA - Multitool aims to be a comprehensive assistant for operations
 and utilities related to system enumeration, vulnerability identification,
 exploitation and privilege escalation. 
 
@@ -31,11 +29,11 @@ main
 
 function init_enum()
 {	clear 2>/dev/null
-	echo " _____         _    _____ _ _   "   
-	echo "| __  |___ ___| |_ |  |  |_| |_ " 
-	echo "| __ -| .'|_ -|   ||    -| |  _|"
-	echo "|_____|__,|___|_|_||__|__|_|_|  "
-	printf "\nBash Kit - Multitool\n"
+	echo " _____ _____ ____  _____ "
+	echo "|     |     |    \|  _  |"
+	echo "| | | |-   -|  |  |     |"
+	echo "|_|_|_|_____|____/|__|__|"
+	printf "\nMIDA - Multitool\n"
 	printf "\n\nThis module enumerates system information and appends it to a textfile.\n"
 	printf "\nThese items will be enumerated: 
 	
@@ -73,9 +71,7 @@ function init_enum()
 	sleep 0.5 && clear 2>/dev/null
 		
 	printf "\n\nOS details and mounted disks\n\n" | tee -a $outfile
-	
-	cat /etc/hostname | tee -a $outfile
-	printf "\n\n" | tee -a $outfile
+		
 	uname -a | tee -a $outfile 1>&2
 	printf "\n\n" | tee -a $outfile
 	dpkg -l linux-image-\* | grep ^ii | tee -a $outfile 1>&2
@@ -90,10 +86,7 @@ function init_enum()
 	arp -e | tee -a $outfile 1>&2
 	printf "\n\n" | tee -a $outfile
 	netstat -atp | tee -a $outfile 1>&2
-	printf "\n\n" | tee -a $outfile 
-        cat /etc/hosts | tee -a $outfile 1>&2
-	printf "\nListening nodes | tee -a $outfile
-	lsof -i | tee -a $outfile 1>&2
+	nodes=&(lsof -i) && printf "\nListening nodes: $nodes\n" | tee -a $outfile
 	printf "\n\n" | tee -a $outfile 
 	iptables -L | tee -a $outfile 1>&2
 	sleep 0.5 && clear 2>/dev/null
@@ -121,11 +114,11 @@ function init_enum()
 
 function common_util()
 {	clear 2>/dev/null
-	echo " _____         _    _____ _ _   "   
-	echo "| __  |___ ___| |_ |  |  |_| |_ " 
-	echo "| __ -| .'|_ -|   ||    -| |  _|"
-	echo "|_____|__,|___|_|_||__|__|_|_|  "
-	printf "\nBash Kit - Multitool\n"
+	echo " _____ _____ ____  _____ "
+	echo "|     |     |    \|  _  |"
+	echo "| | | |-   -|  |  |     |"
+	echo "|_|_|_|_____|____/|__|__|"
+	printf "\nMIDA - Multitool\n"
 	printf "\n\nListing common, available utilities\n\n"
 				
 	which curl 2>/dev/null
@@ -155,11 +148,11 @@ function common_util()
 
 function cleartext()
 {	clear 2>/dev/null
-	echo " _____         _    _____ _ _   "   
-	echo "| __  |___ ___| |_ |  |  |_| |_ " 
-	echo "| __ -| .'|_ -|   ||    -| |  _|"
-	echo "|_____|__,|___|_|_||__|__|_|_|  "
-	printf "\nBash Kit - Multitool\n"
+	echo " _____ _____ ____  _____ "
+	echo "|     |     |    \|  _  |"
+	echo "| | | |-   -|  |  |     |"
+	echo "|_|_|_|_____|____/|__|__|"
+	printf "\nMIDA - Multitool\n"
 	printf "\n\nThis module looks for web application and text files containing certain
 keywords in order to find cleartext passswords and usernames. Currently files ending in 
 '.php', '.sql' and '.txt' will be searched. Results will be saved to a textfile\n\n"
@@ -196,11 +189,11 @@ keywords in order to find cleartext passswords and usernames. Currently files en
 
 function ext_util()
 {	clear 2>/dev/null
-	echo " _____         _    _____ _ _   "   
-	echo "| __  |___ ___| |_ |  |  |_| |_ " 
-	echo "| __ -| .'|_ -|   ||    -| |  _|"
-	echo "|_____|__,|___|_|_||__|__|_|_|  "
-	printf "\nBash Kit - Multitool\n\n"
+	echo " _____ _____ ____  _____ "
+	echo "|     |     |    \|  _  |"
+	echo "| | | |-   -|  |  |     |"
+	echo "|_|_|_|_____|____/|__|__|"
+	printf "\nMIDA - Multitool\n\n"
 	printf "\n\nThis module provides a number of external utilities to download.
 Choose a utility to start downloading and unpacking it in the /tmp/ directory\n"
 	
@@ -218,7 +211,7 @@ Choose a utility to start downloading and unpacking it in the /tmp/ directory\n"
 				wget -O /tmp/firmwalker.zip https://github.com/craigz28/firmwalker/archive/master.zip
 				;;
 			"LUNAR - Unix security auditing") 
-				printf "\n\n"				
+				printf "\n\n"
 				wget -O /tmp/LUNAR.zip https://github.com/lateralblast/lunar/archive/master.zip
 				;;
 			"PyCat - Python network tool")
@@ -240,7 +233,6 @@ Choose a utility to start downloading and unpacking it in the /tmp/ directory\n"
 			"All")
 				wget -O /tmp/LinEnum.zip https://github.com/rebootuser/LinEnum/archive/master.zip
 				wget -O /tmp/firmwalker.zip https://github.com/craigz28/firmwalker/archive/master.zip
-				wget -O /tmp/LUNAR.zip https://github.com/lateralblast/lunar/archive/master.zip
 				wget -O /tmp/PyCat.zip https://github.com/NullArray/PyCat/archive/master.zip
 				wget -O /tmp/ExploitSuggest.py http://www.securitysift.com/download/linuxprivchecker.py
 				wget -O /tmp/ExploitSuggest_perl.zip https://github.com/PenturaLabs/Linux_Exploit_Suggester/archive/master.zip
@@ -253,7 +245,7 @@ Choose a utility to start downloading and unpacking it in the /tmp/ directory\n"
 		esac
 	done
 	
-	printf "\n\nIn case external utilities were downloaded would you like Bash Kit to automatically unzip them?\n\n"
+	printf "\n\nIn case external utilities were downloaded would you like MIDA to automatically unzip them?\n\n"
 	read -p '[Y]es/[N]o: ' choice
 	
 	if [[ $choice == 'y' ]]; then
@@ -292,11 +284,11 @@ Choose a utility to start downloading and unpacking it in the /tmp/ directory\n"
 
 
 function main()
-{	echo " _____         _    _____ _ _   "   
-	echo "| __  |___ ___| |_ |  |  |_| |_ " 
-	echo "| __ -| .'|_ -|   ||    -| |  _|"
-	echo "|_____|__,|___|_|_||__|__|_|_|  "
-	printf "\nBash Kit - Multitool\n\n"
+{	echo " _____ _____ ____  _____ "
+	echo "|     |     |    \|  _  |"
+	echo "| | | |-   -|  |  |     |"
+	echo "|_|_|_|_____|____/|__|__|"
+	printf "\nMIDA - Multitool\n\n"
 	
 	PS3='Please enter your choice: '
 	options=("Usage" "System Enumeration" "Common Utilities" "External Utilities" "Cleartext Credentials" "Quit")
